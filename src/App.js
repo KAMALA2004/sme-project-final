19d652bd1d3b8902bdeac942fc5f56099295974c
kavyakalai <119475596+kavyakalai@users.noreply.github.com>
2025-03-25 13:55:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="*" element={<Navigate to="/" />} /> {/* Catch all other paths */}
+            <Route path="*" element={<Navigate to="/" />} /> {/* Catch all other path */}
           </Routes>
         )}
       </div>
